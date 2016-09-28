adb2768a5b5d3c3f623af0073d808eaffb2c7d06
Tze Hei Tam <18tam.tzehei@gmail.com>
2016-09-28 15:23:43 -0400

    Added base code from last year

---
+++ BadRobot2016Redo/src/src/org/usfirst/frc/team1014/robot/subsystems/BadSubsystem.java
@@ -0,0 +1,29 @@
+
+package org.usfirst.frc.team1014.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An abstract class with the minimums for a subsystem.
+ */
+public abstract class BadSubsystem extends Subsystem
+{
+	protected BadSubsystem()
+	{
+		initialize();
+	}
+
+	/**
+	 * Subclasses should implement their own implementations on initialize. This method is meant to
+	 * instantiate any hardware or variables that will be needed. This is specific to each class and
+	 * can be left blank.
+	 */
+	protected abstract void initialize();
+
+	/**
+	 * @return The String that should appear whenever this Subsystem outputs a String. Can be
+	 *         whatever you want, most likely the class name though.
+	 */
+	public abstract String getConsoleIdentity();
+}
+
