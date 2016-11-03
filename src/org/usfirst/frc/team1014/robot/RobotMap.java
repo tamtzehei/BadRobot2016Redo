a0281ae10c36271b9ae018f1de1e28cb251ed1fb
Dominik Winecki <dominikwinecki@gmail.com>
2016-11-03 19:01:57 -0400

    Add RobotMap

---
+++ src/org/usfirst/frc/team1014/robot/RobotMap.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team1014.robot;
+
+public class RobotMap {
+	/*
+	 * PWM
+	 */
+	public static final int DRIVE_MOTOR_0 = 0;
+	public static final int DRIVE_MOTOR_1 = 1;
+	public static final int DRIVE_MOTOR_2 = 2;
+	public static final int DRIVE_MOTOR_3 = 3;
+	public static final int PIVOT_MOTOR_0 = 4;
+	public static final int PIVOT_MOTOR_1 = 5;
+	public static final int PIVOT_MOTOR_2 = 6;
+	public static final int PIVOT_MOTOR_3 = 7;
+
+	/*
+	 * DIO
+	 */
+	public static final int PIVOT_ENCODER_0A = 0;
+	public static final int PIVOT_ENCODER_0B = 1;
+	public static final int PIVOT_ENCODER_1A = 2;
+	public static final int PIVOT_ENCODER_1B = 3;
+	public static final int PIVOT_ENCODER_2A = 4;
+	public static final int PIVOT_ENCODER_2B = 5;
+	public static final int PIVOT_ENCODER_3A = 6;
+	public static final int PIVOT_ENCODER_3B = 7;
+}
