f110cec81e188ffcc818c29dc34740bea2174298
sgern <sjgern@gmail.com>
2016-11-09 15:17:01 -0500

    Removed swerve drive from RobotMap

--- src/org/usfirst/frc/team1014/robot/RobotMap.java
+++ src/org/usfirst/frc/team1014/robot/RobotMap.java
@@ -8,20 +8,4 @@ public class RobotMap {
 	public static final int DRIVE_MOTOR_1 = 1;
 	public static final int DRIVE_MOTOR_2 = 2;
 	public static final int DRIVE_MOTOR_3 = 3;
-	public static final int PIVOT_MOTOR_0 = 4;
-	public static final int PIVOT_MOTOR_1 = 5;
-	public static final int PIVOT_MOTOR_2 = 6;
-	public static final int PIVOT_MOTOR_3 = 7;
-
-	/*
-	 * DIO
-	 */
-	public static final int PIVOT_ENCODER_0A = 0;
-	public static final int PIVOT_ENCODER_0B = 1;
-	public static final int PIVOT_ENCODER_1A = 2;
-	public static final int PIVOT_ENCODER_1B = 3;
-	public static final int PIVOT_ENCODER_2A = 4;
-	public static final int PIVOT_ENCODER_2B = 5;
-	public static final int PIVOT_ENCODER_3A = 6;
-	public static final int PIVOT_ENCODER_3B = 7;
 }
